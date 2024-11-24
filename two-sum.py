f08d56e6ab9b68c86a65adaac38c117e36dc008f
kihuni <stephenkihuni55@gmail.com>
2024-11-24 05:56:43 +0300

    two sum problem

---
+++ two-sum.py
@@ -0,0 +1,59 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+ 
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+
+# Solution 1: Brute Force
+# Time complexity: O(n^2)
+# Space complexity: O(1)
+
+# def two_sum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i+1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
+
+
+# Solution 2: Using Hash Table
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+def two_sum(nums, target):
+    hash_table = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hash_table:
+            return [hash_table[complement], i]
+        hash_table[nums[i]] = i
+        
+        
+print(two_sum([2,7,11,15,5,4], 9))
